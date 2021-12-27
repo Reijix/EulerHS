dcb7a3fc4e3340af858f8cc76732c1102cad41a6
Leon <leon.vatthauer@web.de>
2021-12-27 16:00:01 +0100

    Solved Problem 6

---
+++ Problem6/sumsquarediff.hs
@@ -0,0 +1,16 @@
+{-
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+-}
+
+main = print (problem 100)
+
+square :: Int -> Int
+square x = x * x
+
+sumTo :: Int -> Int
+sumTo x = sum [1..x]
+
+squareSumTo :: Int -> Int
+squareSumTo x = sum (map (\p -> square p) [1..x])
+
+problem n = square (sumTo n) - squareSumTo n
